607e5000680e6f278d3d142cc970e939a2f265a7
miguel angel zamora montecinos <mzmprog@gmail.com>
2024-02-06 14:59:59 -0300

    Initial commit

---
+++ src/main/java/cl/cachoza/appsmaven/AppsmavenApplication.java
@@ -0,0 +1,13 @@
+package cl.cachoza.appsmaven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppsmavenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppsmavenApplication.class, args);
+	}
+
+}
